51dec26d77f5f49127c97bcb7315daabdbcd10fa
uujuju1 <j4224253@gmail.com>
2022-07-14 23:59:31 -0300

    drawing fix

--- src/funni/graphics/FunniDrawf.java
+++ src/funni/graphics/FunniDrawf.java
@@ -9,26 +9,21 @@ public class FunniDrawf {
 	// very much based from pu
 	public static void rotatingRects(TextureRegion[] regions, int sides, float x, float y, float width, float height, float rot, float rRot) {
 		for (int i = 0; i < sides; i++) {
-			if ((regions[i] instanceof TextureRegion)) {
-				regions[i] = Core.atlas.find("error");
-			}
-		}
-		for (int i = 0; i < sides; i++) {
 			float
-			angle1 = Mathf.mod(rRot, (360/sides) * i),
-			angle2 = Mathf.mod(rRot + 360/sides, (360/sides) * i);
+			angle1 = Mathf.mod(rRot + (360f/sides * i), 360),
+			angle2 = Mathf.mod(rRot + (360f/sides) + (360f/sides * i), 360);
 
-			if (angle1 >= 180 && angle2 >= 180) return;
+			if (angle1 >= 180f && angle2 >= 180f) return;
 			float
 			cos1 = -Mathf.cos(angle1 * Mathf.degreesToRadians),
 			cos2 = -Mathf.cos(angle1 * Mathf.degreesToRadians);
-			if (angle1 > 180) {
+			if (angle1 > 180f) {
 				cos1 = -1;
-			} else if (angle2 > 180) {
+			} else if (angle2 > 180f) {
 				cos2 = 1;
 			}
-			cos1 = Mathf.map(cos1, -1, 1, y - height / 2, y + height / 2);
-			cos2 = Mathf.map(cos2, -1, 1, y - height / 2, y + height / 2);
+			cos1 = Mathf.map(cos1, -1f, 1f, y - height / 2f, y + height / 2f);
+			cos2 = Mathf.map(cos2, -1f, 1f, y - height / 2f, y + height / 2f);
 
 			Draw.rect(regions[i], x, (cos1 + cos2) * 0.5f, width, cos2 - cos1, width * 0.5f, y - cos1, rot);
 		}
